3a1e98738be10c972dad43713f4cc7d537dd47ef
Pravin <yourgithubemail@gmail.com>
2026-03-27 19:19:13 +0530

    Final upload all tasks including Task 10

---
+++ Task-10/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.model.Student;
+import com.example.demo.service.StudentService;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService service;
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student){
+        return service.saveStudent(student);
+    }
+
+    @GetMapping
+    public List<Student> getStudents(){
+        return service.getStudents();
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable int id,@RequestBody Student student){
+        return service.updateStudent(id,student);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable int id){
+        service.deleteStudent(id);
+        return "Student deleted successfully";
+    }
+}
